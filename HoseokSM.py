8f769c7903bebf0834aeab7532ddf1e2b9637c42
ymirluvs <133813161+ymirluvs@users.noreply.github.com>
2024-10-17 13:22:58 +0000

    st.file_uploader

---
+++ HoseokSM.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.file_uploader
+
+st.title ("Hoseok (J-Hope) de BTS finaliza su servicio militar")
+st.header ("Celebrando el regreso de J-Hope")
+st.subheader ("Un momento esperado por ARMY en todo el mundo")
+
+st.text ("Después de haber sido ingresado en abril del 2023, el rapero, bailarín y compositor surcoreano es oficialmente el segundo miembro de BTS que es dado de baja del ejército."
+         "La noticia ha emocionado a millones de fanáticos que esperan con ansias su regreso a las actividades musicales.")
+
+st.video ("Hoseoksaludando.mp4", caption="J-Hope saludando a la prensa y a ARMY al culminar su deber en el servicio militar después de 549 días.", width=300)
+st.text ("Un poco después de las 10:00 AM, hora de Corea del Sur, J-Hope salió de su base militar en donde varios medios de comunicación se encontraban para tomar fotos de este momento."
+         "Esta noticia ha causado revuelo en redes sociales, en donde ARMY ha compartido mensajes de apoyo y alegría, usando hashtags como #OurPieceOfPeaceIsHere y #HOBI_HOMECOMING, además de hacer virales las palabras 'Welcome Back J-Hope'.")
+
+st.video ("HoseokJinabrazo.mp4", caption="El rapero fue recibido por su compañero de grupo Jin.")
+st.text ("Jin, el mayor del grupo, fue el primero en enlistarse el servicio militar y el primero en salir. Apodado por sus fans como 'La luna', fue el encargado de acompañar a 'El sol' en su proceso de salida, recibiéndolo con flores y abrazos.", width=300)
+st.image ("HoseokJinabrazo.mp4", caption="Jin y J-Hope son fotografiados en su reencuentro.", width=300)
+st.text ("Y no solo el rapero fue felicidado por su 'hyung'.")
+st.image ("Namjooninstagram.jpg", caption="RM  | Instagram Story 'Felicitaciones amigo @/uarmyhope. También  iré tras de ti rápidamente.", width=300)
+st.text ("El líder del grupo, Namjoon o conocido popularmente como RM, mostró sus buenos deseos através de instagram. Debido a los horarios, el resto del grupo no pudo asistir al evento del recibimiento de J-Hope.")
